eea205dab7705e491a24742e08b64c3baed43ff0
Катя Фрейдман <katyafreydman@gmail.com>
2022-08-13 18:15:25 +0300

    Added files

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 1500;
+
+        int bonus = ticket / 20;
+        System.out.println("Начислено: " + bonus + " бонусных миль");
+    }
+}
